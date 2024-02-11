15a326fe1bd91ce60f73f04fae2094d541419756
unknown <stan.hordon@gmail.com>
2024-02-11 11:13:12 -0800

    fixed test module

--- Pages/base_page.py
+++ Pages/base_page.py
@@ -14,10 +14,11 @@ reports_directory = r'C:\QA\projects\Charity-and-Sponsorship-Fund\reports'
 class BasePage:
     def __init__(self, driver) -> None:
         self.driver = driver
-        # Header and footer cross all pages 
-        self.header = self.driver.find_element(By.XPATH, '//div[@class="w-section__inner section__inner_16_"]')
-        self.footer = self.driver.find_element(By.XPATH, '//section[@class="footer w-section--footer w-section--no-v-padding"]')
 
+    # Header and footer cross all pages 
+    def find_header_footer(self):    
+        self.header = self.driver.find_element(By.XPATH, '//div[@class="header w-section--header w-section--no-v-padding"]')
+        self.footer = self.driver.find_element(By.XPATH, '//section[@class="footer w-section--footer w-section--no-v-padding"]')    
     # Create a new browser instance   
     def open(self, url):        
         # Full screen browser    
--- Tests/test_home_page.py
+++ Tests/test_home_page.py
@@ -1,12 +1,26 @@
 from Pages.home_page import HomePage
 from selenium import webdriver
 import pytest
+import time
+from openpyxl import Workbook
 
 # Create a new browser instance 
-driver = webdriver.Chrome()
+@pytest.fixture(scope="module")
+def driver():
+    # Setup
+    driver = webdriver.Chrome()
+    yield driver
+    # Teardown
+    driver.quit()
+
 @pytest.mark.parametrize("url", ["https://warvictimsfund.com/"])
-def test_open_page(url):
-     # Full screen browser    
-        driver.maximize_window()   
-        # Open the website  
-        driver.get(url)
+def test_open_page(url, driver):
+      homepage = HomePage(driver)
+      # Full screen browser    
+      homepage.driver.maximize_window() 
+      # Open the website  
+      homepage.driver.get(url)
+
+def test_loading_check(driver):
+    homepage = HomePage(driver)
+    homepage.loading_check()
