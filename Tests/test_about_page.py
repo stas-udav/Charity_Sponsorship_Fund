ef9a36f665ef7530893b6caa935fb52bcb5c32be
unknown <stan.hordon@gmail.com>
2024-02-16 17:26:02 -0800

    allure report integration

--- Tests/test_about_page.py
+++ Tests/test_about_page.py
@@ -28,6 +28,7 @@ def test_email(xpath, driver):
     driver.execute_script("arguments[0].scrollIntoView(true);", email)
     email_value = email.text
     assert email_value == "HELP@WARVICTIMSFUND.COM"
+    allure.attach(driver.get_screenshot_as_png(), name="email", attachment_type=allure.attachment_type.PNG)
     # assert email.click()
     
 # Checking if phone is present and clickable
@@ -39,7 +40,7 @@ def test_phone_present(xpath, driver):
     assert phone_text == "+ 37067045284"
     print(phone_text)
     assert phone.click
-
+    allure.attach(driver.get_screenshot_as_png(), name="Phone", attachment_type=allure.attachment_type.PNG)
 # Checking if business address is correct
 @pytest.mark.parametrize("xpath", ['//span[@style="font-size:18px" and contains(text(), "ŽIRNIŲ STR. 10, VILNIUS 02120, LITHUANIA")]'])
 def test_address(xpath, driver):
@@ -47,5 +48,7 @@ def test_address(xpath, driver):
     address = about_page.find_element(xpath)
     address_value = address.text
     assert address_value == "ŽIRNIŲ STR. 10, VILNIUS 02120, LITHUANIA"
+    allure.attach(driver.get_screenshot_as_png(), name="address", attachment_type=allure.attachment_type.PNG)
+
     print(address_value) 
     
