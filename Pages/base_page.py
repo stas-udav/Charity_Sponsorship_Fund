b20db5f57af492d6fc6facc4ef1efdbd109c2a4e
unknown <stan.hordon@gmail.com>
2024-03-01 17:51:46 -0800

    add variables

--- Pages/base_page.py
+++ Pages/base_page.py
@@ -8,6 +8,12 @@ import re
 import time
 import json
 
+xpath_header = '//div[@class="header w-section--header w-section--no-v-padding"]'
+xpath_footer = '//section[@class="footer w-section--footer w-section--no-v-padding"]'
+xpath_donate = '//a[@data-component="button"][@href="/donate"]'
+xpath_social_media_icon = "//a[@class='social-icons__icon_1W1']"
+xpath_logo = "//img[@class='logo-image_HXE image-logo_cI-']"
+home_page_link = "https://warvictimsfund.com/"
 
 reports_directory = r'C:\QA\projects\Charity-and-Sponsorship-Fund\reports'
 
---
+++ varibles/varebles.py
@@ -0,0 +1,8 @@
+
+class Variables:
+    xpath_header = '//div[@class="header w-section--header w-section--no-v-padding"]'
+    xpath_footer = '//section[@class="footer w-section--footer w-section--no-v-padding"]'
+    xpath_donate = '//a[@data-component="button"][@href="/donate"]'
+    xpath_social_media_icon = "//a[@class='social-icons__icon_1W1']"
+    xpath_logo = "//img[@class='logo-image_HXE image-logo_cI-']"
+    home_page_link = "https://warvictimsfund.com/"
